9effc60c3c8cfef8be7d8cdcd0da19df19fd4716
MrSap8 <64362622+MrSap8@users.noreply.github.com>
2023-07-17 14:01:07 +0700

    Update Beranda.py

--- Beranda.py
+++ Beranda.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.markdown("""
     <style>
@@ -12,3 +13,6 @@ st.markdown("""
 st.markdown('<h1 class="title">SkorPlus</h1>', unsafe_allow_html=True)
 st.write('SkorPlus adalah website untuk prediksi hasil pertandingan sepakbola dengan menggunakan metode AdaBoost. Pada website ini juga terdapat performa dari metode XGBoost, LightGBM, dan AdaBoost untuk prediksi hasil pertandingan sepakbola')
 
+image = Image.open('gambar/gambar.jpg')
+resized_image = image.resize((2000, 1000))
+st.image(resized_image, caption='', use_column_width=True)
